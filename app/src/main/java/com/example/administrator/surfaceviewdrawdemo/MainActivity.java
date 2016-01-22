d57375483e8b88819c6617c4eee225491629ece4
OldChickenX <15552447763@163.com>
2016-01-22 09:53:42 +0800

    正弦曲线

--- app/src/main/java/com/example/administrator/surfaceviewdrawdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/surfaceviewdrawdemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.administrator.surfaceviewdrawdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 正弦曲线
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
