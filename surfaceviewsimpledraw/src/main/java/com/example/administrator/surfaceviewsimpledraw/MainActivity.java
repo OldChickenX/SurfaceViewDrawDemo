a7525c434f59f86d8b4ef8f9b291a9880e9e2daf
OldChickenX <15552447763@163.com>
2016-01-22 09:55:37 +0800

    surfaceView刷新自己手指滑过的图案

--- surfaceviewsimpledraw/src/main/java/com/example/administrator/surfaceviewsimpledraw/MainActivity.java
+++ surfaceviewsimpledraw/src/main/java/com/example/administrator/surfaceviewsimpledraw/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.administrator.surfaceviewsimpledraw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * surfaceView画自己手指划过的内容
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
